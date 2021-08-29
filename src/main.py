c757c686acbc1df68702339d17b570ebbe0a07a3
igna <korea@seized.email>
2021-08-29 17:45:55 +0000

    few changes

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+from nextcord.ext import commands
+
+TOKEN = os.environ['TOKEN']
+PREFIX = os.environ['PREFIX']
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event()
+async def on_ready():
+    print(f'{bot.user} is online!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.reply('Pong!')
+
+bot.run(token)
